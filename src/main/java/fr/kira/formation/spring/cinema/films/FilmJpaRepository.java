20dd28d12a9cfa24f0fdaad46cf4362a000eb97e
Cloe <cloe.blanchard83@gmail.com>
2023-01-05 16:41:52 +0100

    liste des film a une date donnée

--- src/main/java/fr/kira/formation/spring/cinema/films/FilmJpaRepository.java
+++ src/main/java/fr/kira/formation/spring/cinema/films/FilmJpaRepository.java
@@ -1,13 +1,15 @@
 package fr.kira.formation.spring.cinema.films;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
 /**
  * Repository pour la gestion des films en utilisant Spring Boot data JPA.
- * Ici il y a deux types générique:
+ * Ici, il y a deux types génériques :
  * - Film qui definie que cette interface gère les films
  * - Integer qui est le type de la clé primaire des films.
  */
@@ -17,6 +19,7 @@ public interface FilmJpaRepository extends JpaRepository<Film, Integer> {
 
     List<Film> findByTitreContainingIgnoreCaseAndDureeBetween(String titre, int dureeStart, int dureeEnd);
 
-    List<Film> findByDateSortie(LocalDate date_sotie);
+    @Query("SELECT f FROM Film f LEFT JOIN Seance s WHERE s.date = :date")
+    List<Film> findByDate(@Param("date") LocalDate date);
 
 }
